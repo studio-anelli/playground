134b59e0432707f4800958296e8b2485261a2077
studio-anelli <hello@anelli.studio>
2026-09-20 18:49:11 +0200

    Make experiment back navigation reliable

--- app/experiments/gp888-drum-machine/page.tsx
+++ app/experiments/gp888-drum-machine/page.tsx
@@ -4,7 +4,8 @@ export default function GP888DrumMachinePage() {
   return (
     <main className="import-page">
       <header className="import-header">
-        <a href="/" className="back-link"><ArrowLeft aria-hidden="true" /> Index</a>
+        {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+        <a href="/" target="_top" className="back-link"><ArrowLeft aria-hidden="true" /> Index</a>
         <h1>GP888 Drum Machine</h1>
         <span>REC-05 / Tool / Test</span>
         <a className="fullscreen-link" href="/experiments/gp888-drum-machine/app">
