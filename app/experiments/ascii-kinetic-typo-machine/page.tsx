d51b70fce493e43dd4d72b99a3241bb1f73e1602
studio-anelli <hello@anelli.studio>
2026-09-20 18:48:53 +0200

    Make experiment back navigation reliable

--- app/experiments/ascii-kinetic-typo-machine/page.tsx
+++ app/experiments/ascii-kinetic-typo-machine/page.tsx
@@ -8,7 +8,7 @@ export default function ASCIITypoMachinePage() {
       <header className="ui-v1-header mix-blend-difference">
         {/* A full-page link is more reliable than client navigation in the Vinext worker build. */}
         {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-        <a href="/" className="ui-v1-back"><ArrowLeft aria-hidden="true" /> Back</a>
+        <a href="/" target="_top" className="ui-v1-back"><ArrowLeft aria-hidden="true" /> Back</a>
         <h1>ASCII Kinetic Typo Machine</h1>
         <div className="ui-v1-meta">
           <span>REC - 006</span>
