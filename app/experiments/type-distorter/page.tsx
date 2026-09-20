1e6c15fe8a397d242cf703a359e4b0df94cdf3ea
studio-anelli <hello@anelli.studio>
2026-09-20 22:05:47 +0200

    Force REC-02 route edge to edge

--- app/experiments/type-distorter/page.tsx
+++ app/experiments/type-distorter/page.tsx
@@ -5,7 +5,7 @@ import TypeDistorter from "@/recovered/type-distorter-original/particle-type-dis
 
 export default function TypeDistorterPage() {
   return (
-    <main className="import-page w-full max-w-none mx-0">
+    <main className="import-page" style={{ width: "100%", maxWidth: "none", margin: 0 }}>
       <header className="import-header import-header-native">
         {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
         <a href="/" target="_top" className="back-link"><ArrowLeft aria-hidden="true" /> Index</a>
