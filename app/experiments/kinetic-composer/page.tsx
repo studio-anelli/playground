5c28d6f7d69708c8b75092a747cad88e8b80ffc5
studio-anelli <hello@anelli.studio>
2026-09-20 18:49:19 +0200

    Make experiment back navigation reliable

--- app/experiments/kinetic-composer/page.tsx
+++ app/experiments/kinetic-composer/page.tsx
@@ -9,7 +9,7 @@ export default function KineticComposerPage() {
     <main className="ui-v1-page" style={{ "--ui-v1-bg": "#0b0b0b", "--ui-v1-fg": "#f2f2f2" } as CSSProperties}>
       <header className="ui-v1-header">
         {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
-        <a href="/" className="ui-v1-back"><ArrowLeft aria-hidden="true" /> Back</a>
+        <a href="/" target="_top" className="ui-v1-back"><ArrowLeft aria-hidden="true" /> Back</a>
         <h1>Kinetic Composer</h1>
         <div className="ui-v1-meta">
           <span>REC - 001</span>
